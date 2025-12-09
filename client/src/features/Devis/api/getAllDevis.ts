85237f96b4474941e8da8ae5b951a9d3a60f1672
Paul Deblicker <paul.deblicker@greenscope.io>
2025-12-09 22:55:47 +0100

    fix: add delay in fetchAllDevis in order to display loader (for fun)

--- client/src/features/Devis/api/getAllDevis.ts
+++ client/src/features/Devis/api/getAllDevis.ts
@@ -1,8 +1,15 @@
 import { useQuery } from '@tanstack/react-query';
 import { Devis } from '../types';
 
+
+const delay = (ms: number): Promise<void> =>
+  new Promise((resolve) => setTimeout(resolve, ms));
+
 const fetchAllDevis = async (): Promise<Devis[]> => {
   const response = await fetch('http://127.0.0.1:3001/devis');
+
+  await delay(1000);
+
   return response.json();
 };
 
