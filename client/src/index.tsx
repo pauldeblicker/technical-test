ae784db6ddcd00ed93a30157aa65d6ebf9a965c0
Paul Deblicker <paul.deblicker@greenscope.io>
2025-12-09 22:46:43 +0100

    chore(client): fix formatting and clean up imports in index.tsx

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,21 +1,20 @@
-import React from 'react';
-import './index.css';
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
 import { App } from './App';
 import reportWebVitals from './reportWebVitals';
-import {createRoot} from 'react-dom/client';
+import './index.css';
 
 const element = document.getElementById('root');
 
-if(element) {
+if (element) {
   const root = createRoot(element);
 
   root.render(
-    <React.StrictMode>
+    <StrictMode>
       <App />
-    </React.StrictMode>
+    </StrictMode>
   );
 }
-
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
