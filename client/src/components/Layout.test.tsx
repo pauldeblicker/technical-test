2fa1620c45f0f857477a58bccac3c866c64c2843
Paul Deblicker <paul.deblicker@greenscope.io>
2025-12-09 23:05:32 +0100

    refactor(client): co-locate tests with components

--- client/src/components/Layout.test.tsx
+++ client/src/components/Layout.test.tsx
@@ -1,6 +1,6 @@
 import { render, screen } from '@testing-library/react';
 import { MemoryRouter } from 'react-router-dom';
-import { Layout } from '../components/Layout';
+import { Layout } from './Layout';
 
 describe('<Layout />', () => {
   const renderWithRouter = () => {
