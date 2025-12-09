9c43a67ae994f40bfd156b5b3d619f95b7e91186
Paul Deblicker <paul.deblicker@greenscope.io>
2025-12-09 17:57:03 +0100

    chore(server): remove superfluous comments and clean up formatting

--- server/src/modules/devis/useCases/getAllDevis/index.ts
+++ server/src/modules/devis/useCases/getAllDevis/index.ts
@@ -2,15 +2,7 @@ import { devisRepository } from '../../repositories';
 import { GetAllDevisUseCaseController } from './getAllDevisController';
 import { GetAllDevisUseCase } from './GetAllDevisUseCase';
 
-const getAllDevisUseCase = new GetAllDevisUseCase(
-  devisRepository,
-);
+const getAllDevisUseCase = new GetAllDevisUseCase(devisRepository);
+const getAllDevisUseCaseController = new GetAllDevisUseCaseController(getAllDevisUseCase);
 
-// Inject the use case into the controller to create it
-const getAllDevisUseCaseController = new GetAllDevisUseCaseController(
-  getAllDevisUseCase,
-);
-
-export {
-  getAllDevisUseCaseController,
-};
+export { getAllDevisUseCaseController };
